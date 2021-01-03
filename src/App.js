51f9b066a5a5954ccbc8df7ea68bbdb1be514145
Özgür SARIOĞLAN <ozgursarioglan@hotmail.com>
2021-01-03 18:28:04 +0300

    hatalar giderildi

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -8,10 +7,10 @@ function App() {
           <img src="https://instela-static.info/s/images/instela-desktop@2x.png" alt=""/>
           <br/>
         <p>
-           bu playlist sayın <a target="_blank" href="https://tr.instela.com/user/ekmek-arasi-maden-suyu--173406" type="button" className="btn btn-light">ekmek arası maden suyu</a> instela yazarına hediyemdir.
+           bu playlist sayın <a href="https://tr.instela.com/user/ekmek-arasi-maden-suyu--173406" type="button" className="btn btn-light">ekmek arası maden suyu</a> için hazırlanmıştır.
         </p>
           <br/>
-          <iframe src="https://open.spotify.com/embed/playlist/2hsqMXAzjjhKHlgVzIdrAb" width="600" height="380"
+          <iframe title="EAMS" src="https://open.spotify.com/embed/playlist/2hsqMXAzjjhKHlgVzIdrAb" width="600" height="380"
     frameBorder="0" allowTransparency="true" allow="encrypted-media"/>
       </header>
     </div>
