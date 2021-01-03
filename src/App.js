671c2fc20021bc5ac86c60960c1991e91b3fce42
Özgür SARIOĞLAN <ozgursarioglan@hotmail.com>
2021-01-03 18:17:13 +0300

    project created

--- src/App.js
+++ src/App.js
@@ -5,18 +5,14 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+          <img src="https://instela-static.info/s/images/instela-desktop@2x.png" alt=""/>
+          <br/>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+           bu playlist sayın <a target="_blank" href="https://tr.instela.com/user/ekmek-arasi-maden-suyu--173406" type="button" className="btn btn-light">ekmek arası maden suyu</a> instela yazarına hediyemdir.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <br/>
+          <iframe src="https://open.spotify.com/embed/playlist/2hsqMXAzjjhKHlgVzIdrAb" width="600" height="380"
+    frameBorder="0" allowTransparency="true" allow="encrypted-media"/>
       </header>
     </div>
   );
